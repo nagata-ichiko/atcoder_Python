4d25653f3a159ca071a56c373fad85d52f95e62a
nagata-ichiko <nagataichiko2249@gmail.com>
2023-02-12 22:26:08 +0900

    289 A

---
+++ 289/A.py
@@ -0,0 +1,6 @@
+s = input().replace("0","2")
+s = s.replace("1", "3")
+s = s.replace("2", "1")
+s = s.replace("3", "0")
+
+print(s)
--- code/work.py
+++ code/work.py
@@ -1,18 +1,16 @@
-import io
-import sys
-import statistics
-import math
-import queue
-import heapq
+n, m = map(int, input().split())
+li = list(map(int, input().split()))
+result = []
+for i in range(1,n+1):
+    result.append(str(i))  
 
-num = int(input())
-s = input()
+for i in li:
+    result[i-1],result[i] = result[i],result[i-1]
 
-ans = 0
-a = "ABC"
-for i in range(0,num-2):
-    if s[i] == "A":
-        if s[i+1] == "B":
-            if s[i+2] == "C":
-                ans+=1
-print(ans)
+
+sss= ""
+for i in result:
+    sss += i
+    sss += " "
+    
+print(sss)
