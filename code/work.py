d4192ad7681cba1fe744f74b810b8fea92f24c7c
nagata-ichiko <eiichiro.kinoshita@i-standard.jp>
2023-12-01 13:49:08 +0900

    no message

--- code/work.py
+++ code/work.py
@@ -1,25 +1,32 @@
-N, M = map(int, input().split())
-dict = {}
-for i in range(1,N):
-    dict[i] = []
+from collections import deque
+quee = deque()
+R, C = map(int, input().split())
+sy,sx = map(int, input().split())
+gy,gx = map(int, input().split())
 
-for i in range(M):
-    a, b = map(int, input().split())
-    dict.setdefault(a, []).append(b)
-    dict.setdefault(b, []).append(a)
-Mastercount = 0
-# キーで辞書をソート
-sortDict = sorted(dict.items(), key=lambda x: x[0])
-for key, value in sortDict:
-    count = 0
-    for v in value:
-        if  v < key:
-            if count != 0:
-                Mastercount -= 1
-                count = 0
-                break
-        if  key > v:
-            count += 1
-            Mastercount += 1
+# 移動距離
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+nodeList = []
+nodeValueList = []
+for i in range(R):
+    nodeList.append(list(input()))
 
-print(Mastercount)
+nodeValueList = [[(0) for x in range(C)] for y in range(R)]
+quee.append((sy-1,sx-1,0))
+count = 1
+while len(quee) > 0:
+    y, x,d = quee.popleft()
+    if y == gy-1 and x == gx-1:
+        # print(y,x,d)
+        print(nodeValueList[y][x])
+        break
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if nodeList[ny][nx] == '.':
+            if nodeValueList[ny][nx] == 0:
+                nodeValueList[ny][nx] = d+1
+                quee.append((ny,nx,d+1))
+
+# print(nodeValueList)
