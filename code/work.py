9fd7508c050f306ab16aa09d1356e771d49eabc0
nagata-ichiko <eiichiro.kinoshita@i-standard.jp>
2024-04-08 14:49:12 +0900

    C

--- code/work.py
+++ code/work.py
@@ -1,8 +1,16 @@
-S = input().strip()
-substrings = set()
-for i in range(len(S)):
-    for j in range(i + 1, len(S) + 1):
-        substrings.add(S[i:j])
-        
-print((substrings))
+N = int(input())
+minimu_dict = {}
 
+for i in range(0, N):
+    A,C = map(int, input().split())
+    if C in minimu_dict:
+        minimu_dict[C] = min(minimu_dict[C], A)
+    else:
+        minimu_dict[C] = A
+ans =-1
+
+# 全検索 
+for i in minimu_dict.values():
+    ans = max(ans, i)
+    
+print(ans)
